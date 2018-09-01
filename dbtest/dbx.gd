6c3621b7bd8288c61c63aa8a2299e62423075e52
sxkod <sreekant.kodela@googlemail.com>
2018-09-01 16:33:37 -0500

    Add files via upload

---
+++ dbtest/dbx.gd
@@ -0,0 +1,106 @@
+extends Node
+
+#USAGE: See dbtest.gd
+#LICENSE: MIT
+#VERSION: 0.1
+#DEV: S Kodela
+
+#TODO : 
+# - load data to make a new dbx object
+# - facility to load and save data??
+# - 
+#
+
+class dbx:
+
+	var db={}
+	
+	func _init():
+		pass
+
+	func insert(key,data):
+		db[key]=data
+
+	func get(key):
+		if key in db:
+			return db[key]
+		else:
+			return null
+			
+	func mget(xkeys):
+		var res=[]
+		for n in db.keys():
+			if n in xkeys:
+				res.append(db[n])
+		return res
+		
+	func remove(key):
+		if key in db:
+			db.erase(key)
+
+	func mremove(keys):
+		for key in keys:
+			if key in db:
+				db.erase(key)
+
+
+	func load(data):
+		pass
+
+	func query(params):
+		var res=[]
+		var restmp=[]
+		
+		for n in params:
+			res.append(testcond(n))
+
+		for n in range(len(params)):
+			if params[n][0]=="or":
+				restmp.append(res[n])
+			elif params[n][0]=="and":
+				if len(res[n])>0:
+					if len(restmp)>0:
+						#now test intersections - to be done
+						var lastone=restmp[len(restmp)-1]
+						var thisone=res[n]						
+						if len(restmp[len(restmp)-1])>0:
+							restmp.remove(len(restmp)-1)
+							restmp.append(overlap_lists(lastone,thisone))
+					else:
+						restmp.append(res[n])
+		return flatten(restmp)
+
+	func overlap_lists(l1,l2):
+		var ret={}
+		for n in l1:
+			if n in l2:
+				ret[n]=true
+		for n in l2:
+			if n in l1:
+				ret[n]=true
+		return ret.keys()
+
+	func flatten(data):
+		var ret={}
+		for n in data:
+			for m in n:
+				ret[m]=true
+		return ret.keys()
+		
+	func testcond(cond):
+		var res=[]
+		for n in db.keys():
+			if cond[2]=="<":
+				if db[n][cond[1]]<cond[3]:res.append(n)
+			elif cond[2]=="<=":
+				if db[n][cond[1]]<=cond[3]:
+					res.append(n)
+			elif cond[2]=="==":
+				if db[n][cond[1]]==cond[3]:res.append(n)
+			elif cond[2]==">":
+				if db[n][cond[1]]>cond[3]:res.append(n)
+			elif cond[2]==">=":
+				if db[n][cond[1]]>=cond[3]:res.append(n)
+		return res
+		
+		
