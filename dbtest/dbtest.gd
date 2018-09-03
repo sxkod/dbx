7a57b7c6bb5ce7b9c952177408a3fe396b098f50
sxkod <sreekant.kodela@googlemail.com>
2018-09-02 21:29:06 -0500

    Add files via upload

--- dbtest/dbtest.gd
+++ dbtest/dbtest.gd
@@ -1,19 +1,58 @@
 extends Node
 
+
+#PRELOAD the dbx - now the variable dbxfile is the dbx class
 var dbxfile=preload("res://dbx.gd")
 
 func _ready():
-	var x=dbxfile.dbx.new()
+	
+	#create a new object of type dbx
+	var x=dbxfile.new()
+
+	#lets put some data there	
 	x.put("dude1",{"age":45,"ht":5.1,"wt":190})
 	x.put("dude2",{"age":46,"ht":5.2,"wt":200})
 	x.put("dude3",{"age":47,"ht":5.3,"wt":210})
 	x.put("dude4",{"age":48,"ht":5.4,"wt":220})
 	x.put("dude5",{"age":49,"ht":5.5,"wt":230})
-	
+	print("listing all entries : ",x.list())
+
+	#delete one item
 	x.del("dude4")
-	print("getting the deleted ",x.get("dude4"))
+	print("after deleting dude4 : ",x.list())
+	#put back the poor dude4
+	x.put("dude4",{"age":48,"ht":5.4,"wt":220})
+
+	#get one entry
+	print("dude2 is ",x.get("dude2"))
+	
+	#get multiple entries
+	print("some dudes are ",x.get(["dude1","dude2","dude3"]))
+
+	#delete multiple items
+	x.del(["dude1","dude2"])
+
+	#put them back
+	x.put("dude1",{"age":45,"ht":5.1,"wt":190})
+	x.put("dude2",{"age":46,"ht":5.2,"wt":200})
+
+
+	#query the database
 	var res=x.query([  ["and","ht","!=",5.3], ["and","ht","!=",5.5] ])
-	print("float ",res)
+	print("got these  ",res)
+
+
+	#create a database directly by telling the db to use our own dict
+	var tdict={}
+	tdict["duder1"]={"name":"duder1","xid":1}
+	tdict["duder2"]={"name":"duder2","xid":2}
+	tdict["duder3"]={"name":"duder3","xid":3}
+	tdict["duder4"]={"name":"duder4","xid":4}
+	x.create(tdict)
+	print("our new database from the dict ",x.list())
+	res=x.query([  ["and","xid",">=",2] ])
+	print("got these from new db ",res)
+
 
 
 func _process(delta):
--- dbtest/dbx.gd
+++ dbtest/dbx.gd
@@ -2,7 +2,7 @@ extends Node
 
 #USAGE: See dbtest.gd
 #LICENSE: MIT
-#VERSION: 0.1.2
+#VERSION: 0.1.4
 #DEV: S Kodela
 
 #TODO : 
@@ -11,119 +11,134 @@ extends Node
 # - 
 #
 #Changes:
-#changed insert() to put() to keep the wording consistent - 
-# get(), put(), mget(), del(), mdel() etc
-#changed load to create, to remove confusion from native load reserved word
+# now the class is implied to make syntax clear and more godot like
+# changed insert() to put() to keep the wording consistent - 
+# now the main methods are get(), put(), del(), list(), query()
+# changed load to create, to remove confusion from native load reserved word
 
-class dbx:
 
-	var db={}
-	const EPSILON_VALUE=0.001
-	const FLOAT_FORMAT = "%0.4f"
+var db={}
+const EPSILON_VALUE=0.001
+const FLOAT_FORMAT = "%0.4f"
 
-	func _init():
-		pass
-
-	func put(key,data):
+func put(key,data):
+	if typeof(data)==TYPE_DICTIONARY:
 		db[key]=data
+		return true
+	else:
+		return false
 
-	func get(key):
+func get(key):
+	if typeof(key)==TYPE_ARRAY:
+		return mget(key)
+	else:
 		if key in db:
 			return db[key]
 		else:
 			return null
 
-	func mget(xkeys):
-		var res=[]
-		for n in db.keys():
-			if n in xkeys:
-				res.append(db[n])
-		return res
+func mget(xkeys):
+	var res=[]
+	for n in db.keys():
+		if n in xkeys:
+			res.append(db[n])
+	return res
 
-	func del(key):
+func list():
+	return db.keys()
+	
+func del(key):
+	if typeof(key)==TYPE_ARRAY:
+		mdel(key)
+	else:
 		if key in db:
 			db.erase(key)
 
-	func mdel(keys):
-		for key in keys:
-			if key in db:
-				db.erase(key)
+			
+func mdel(keys):
+	for key in keys:
+		if key in db:
+			db.erase(key)
+		
 
-
-	func create(data):
+func create(data):
+	if typeof(data)==TYPE_DICTIONARY:
 		db=data
+		return true
+	else:
+		return false
 
-	func query(params):
-		var res=[]
-		var restmp=[]
-		
-		for n in params:
-			res.append(testcond(n))
+func query(params):
+	var res=[]
+	var restmp=[]
+	
+	for n in params:
+		res.append(testcond(n))
 
-		for n in range(len(params)):
-			if params[n][0]=="or":
-				restmp.append(res[n])
-			elif params[n][0]=="and":
-				if len(res[n])>0:
-					if len(restmp)>0:
-						var lastone=restmp[len(restmp)-1]
-						var thisone=res[n]						
-						if len(restmp[len(restmp)-1])>0:
-							restmp.remove(len(restmp)-1)
-							restmp.append(overlap_lists(lastone,thisone))
-					else:
-						restmp.append(res[n])
-		return flatten(restmp)
+	for n in range(len(params)):
+		if params[n][0]=="or":
+			restmp.append(res[n])
+		elif params[n][0]=="and":
+			if len(res[n])>0:
+				if len(restmp)>0:
+					var lastone=restmp[len(restmp)-1]
+					var thisone=res[n]						
+					if len(restmp[len(restmp)-1])>0:
+						restmp.remove(len(restmp)-1)
+						restmp.append(overlap_lists(lastone,thisone))
+				else:
+					restmp.append(res[n])
+	return flatten(restmp)
 
-	func overlap_lists(l1,l2):
-		var ret={}
-		for n in l1:
-			if n in l2:
-				ret[n]=true
-		for n in l2:
-			if n in l1:
-				ret[n]=true
-		return ret.keys()
+func overlap_lists(l1,l2):
+	var ret={}
+	for n in l1:
+		if n in l2:
+			ret[n]=true
+	for n in l2:
+		if n in l1:
+			ret[n]=true
+	return ret.keys()
 
-	func flatten(data):
-		var ret={}
-		for n in data:
-			for m in n:
-				ret[m]=true
-		return ret.keys()
+func flatten(data):
+	var ret={}
+	for n in data:
+		for m in n:
+			ret[m]=true
+	return ret.keys()
 
-	func testcond(cond):
-		var res=[]
-		var tst=cond[3]
-		var op=cond[2]
-		
-		for n in db.keys():
-			var fld=db[n][cond[1]]
+func testcond(cond):
+	var res=[]
+	var tst=cond[3]
+	var op=cond[2]
+	
+	for n in db.keys():
+		var fld=db[n][cond[1]]
 
-			if typeof(tst)==TYPE_REAL and typeof(fld)==TYPE_REAL:
-				fld=FLOAT_FORMAT % fld
-				tst=FLOAT_FORMAT % tst
-			else:
-				tst=cond[3]
+		if typeof(tst)==TYPE_REAL and typeof(fld)==TYPE_REAL:
+			fld=FLOAT_FORMAT % fld
+			tst=FLOAT_FORMAT % tst
+		else:
+			tst=cond[3]
 
-			if op=="<":
-				if fld<tst:res.append(n)
+		if op=="<":
+			if fld<tst:res.append(n)
 
-			elif op=="<=":
-				if fld<=tst:res.append(n)
+		elif op=="<=":
+			if fld<=tst:res.append(n)
 
-			elif op=="==":
-				if fld==tst:res.append(n)
+		elif op=="==":
+			if fld==tst:res.append(n)
 
-			elif op==">":
-				if fld>tst:res.append(n)
+		elif op==">":
+			if fld>tst:res.append(n)
 
-			elif op==">=":
-				if fld>=tst:res.append(n)
+		elif op==">=":
+			if fld>=tst:res.append(n)
 
-			elif op=="!=":
-				if fld!=tst:res.append(n)
+		elif op=="!=":
+			if fld!=tst:res.append(n)
 
-		return res
+	return res
 
 
